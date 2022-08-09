1d379d8f40da49403de4fc5975a18f498dfa022d
Daniel Barrera <dbarrera.ap@gmail.com>
2022-08-09 17:48:27 -0400

    termino api

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout"
 
-const Blog = () => {
+const Blog = ({entradas}) => {
+    console.log(entradas);
     return (
         <Layout
             pagina='Nosotros'
@@ -8,6 +9,18 @@ const Blog = () => {
             <h1>Desde Blog</h1> 
         </Layout>
     )
+}
+
+export async function getServerSideProps(){
+    const url = 'http://localhost:1337/blogs'
+    const respuesta = await fetch(url)
+    const entradas = await respuesta.json()
+
+    return {
+        props: {
+            entradas
+        }
+    }
 }
 
 export default Blog
