c9c8d529420deb7222584552de2db96fe87e60a0
Daniel Barrera <dbarrera.ap@gmail.com>
2022-08-10 16:06:01 -0400

    avance consumir api

--- pages/blog.js
+++ pages/blog.js
@@ -1,21 +1,32 @@
 import Layout from "../components/Layout"
+import Entrada from "../components/Entrada"
 
 const Blog = ({entradas}) => {
-    console.log(entradas);
     return (
         <Layout
             pagina='Nosotros'
         >
-            <h1>Desde Blog</h1> 
+            <main className="contenedor">
+                <h2 className="heading">Blog</h2>
+                <div>
+                    {
+                        entradas.map(entrada=>(
+                        <Entrada
+                            key={entrada.id}
+                            entrada={entrada}
+                        />
+                        ))
+                    }
+                </div>
+            </main> 
         </Layout>
     )
 }
 
-export async function getServerSideProps(){
+export async function getStaticProps(){
     const url = 'http://localhost:1337/blogs'
     const respuesta = await fetch(url)
     const entradas = await respuesta.json()
-
     return {
         props: {
             entradas
